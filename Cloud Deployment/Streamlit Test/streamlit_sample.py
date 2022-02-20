b592883b6849c3aafce2b88c381d5400b0b32b3e
Tim Coelho <timothycoelho@gmail.com>
2022-02-20 17:06:24 -0500

    Update streamlit_sample.py

--- Cloud Deployment/Streamlit Test/streamlit_sample.py
+++ Cloud Deployment/Streamlit Test/streamlit_sample.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
-# py -m streamlit run streamlit_sample.py to deploy (Windows)
+# py -m streamlit run streamlit_sample.py to deploy (Windows) or you can remove the py -m if you set env var to [PYTHON_PATH]/Scripts
 
 # Image path
 path = os.environ["DATA_PATH"] + "/images/animals/dog.jpg"
